535d0edc29fc275296158f0c59ebb2b38451b6c6
Rodrigo <moura.desenvolvimento@gmail.com>
2020-10-21 12:48:55 -0300

    melhora de rotas e de controllers

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -2,3 +2,6 @@ exports.paginaInicial = (req, res) => {
     res.send(
         '<form action="/" method="POST"> Nome: <input type="text" name="nome"> <button>Enviar</button> </form>')
 };
+exports.trataPOST = (req,res)=>{
+    res.send('sou sua nova rota de post');
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const route = express.Router();
 const homeController = require('./Controllers/homeController')
+const contatoController = require('./Controllers/contatoController')
 
 route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPOST)
+route.get ('/contato', contatoController.paginaInicial)
 
 module.exports = route;
