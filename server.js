70c43a2ea7f84f6787b41326a0b43ead6d7effd1
Rodrigo <moura.desenvolvimento@gmail.com>
2020-10-21 12:09:28 -0300

    'requisicao-tratando-metodos-send-post'

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req,res) =>{
-res.send('Hello World')
+res.send(
+    '<form action="/" method="POST"> Nome: <input type="text" name="nome"> <button>Enviar</button> </form>')
+})
+app.get('/testes/:idUsuarios?/:parametro?', (req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params);
 })
 app.listen(3000, ()=>{
     console.log('Acessar http://localhost:3000')
@@ -12,5 +20,9 @@ app.get('/contato', (req,res)=>{
     res.send('Page de contatos')
 
 });
+app.post('/', (req,res)=>{
+    console.log(req.body);
+    res.send(`Formulário enviado com sucesso nome enviado foi: ${req.body.nome}`)
+})
 
 
