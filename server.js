012d396a39a4d6266bb8f078d222ff7ee6780265
Rodrigo <moura.desenvolvimento@gmail.com>
2020-10-21 10:54:34 -0300

    'atualicao-com-rotas'

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const a = 'a'
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) =>{
+res.send('Hello World')
+})
+app.listen(3000, ()=>{
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor Executando na porta 3000')
+});
+app.get('/contato', (req,res)=>{
+    res.send('Page de contatos')
+
+});
+
+
